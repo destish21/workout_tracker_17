ede5edf86c40e99085ffc34651e72deca08b1131
desta mulu <destish21@yahoo.com>
2021-03-12 23:38:30 -0600

    deploying

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static('public'));
 const db = require("./models");
 
 // const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost/workout';
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/workout', 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', 
     {
     useNewUrlParser: true,
     useUnifiedTopology: true,
