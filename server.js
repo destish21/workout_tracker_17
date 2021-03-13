22d0fb4937b37116b43097a65854a78faf40474f
desta mulu <destish21@yahoo.com>
2021-03-13 14:55:49 -0600

    deploying

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout',
     {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+    // useCreateIndex: true,
+    // useFindAndModify: false
 });
 
  require('./routes/apiRoutes')(app);
